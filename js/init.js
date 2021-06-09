7322e11f73c1e5710d6fe14253108b0cafb980ef
epham36 <emilypham36@gmail.com>
2021-06-08 21:08:27 -0700

    Co-authored-by: albertkun <albertkun@users.noreply.github.com>

--- js/init.js
+++ js/init.js
@@ -8,6 +8,8 @@ let didFeelAffirmed = L.featureGroup();
 let maybeAffirmed = L.featureGroup(); 
 let notAffirmed = L.featureGroup(); 
 
+
+
 let allMapLayers = {
   "Felt affirmed": didFeelAffirmed, 
   "Maybe affirmed": maybeAffirmed, 
@@ -15,8 +17,12 @@ let allMapLayers = {
 }
 
 const formattedData = [] /* this array will eventually be populated with the contents of the spreadsheet's rows */; 
-
+const formattedClubData =[];  // this array is for the cleaned club data
 let url = "https://spreadsheets.google.com/feeds/list/16vj49I0vAirmk54P13zE-V70Je4uq9G3jABb8T8OHA0/oxhqdjk/public/values?alt=json"; 
+let cluburl = "https://spreadsheets.google.com/feeds/list/16vj49I0vAirmk54P13zE-V70Je4uq9G3jABb8T8OHA0/oo1fzto/public/values?alt=json";
+
+getClubData(cluburl)
+
 
 fetch(url)
 	.then(response => {
@@ -72,7 +78,109 @@ function makePopup(data, name) //Todo: map each letter to a descriptive name in 
     
 //     buttonPanel.appendChild(newButton); //this adds the button to our page.
 //   }
+// function duplicateClubs(club){
 
+// }
+function getClubs(club,lookup){
+  let clubKey = club.keyword
+  // console.log(club)
+  // console.log(lookup)
+  if (clubKey == lookup){
+    let clubName = club.name
+    return clubName
+
+  }
+  
+}
+
+
+function createClubButton(lookup,id){
+  // console.log(formattedClubData)
+  console.log(lookup)
+
+  let clubValues = formattedClubData.forEach(club => getClubs(club,lookup))
+  // console.log(clubValues)
+  // for (let clubDictionary in formattedClubData) {
+  //   console.log(clubDictionary)
+  //   // console.log(formattedClubData[i] + " ");
+  // }
+  if (lookup !="#N/A"){
+    const newClub = document.createElement("button");
+    newClub.innerHTML = lookup
+    // newOrg.innerHTML = '<div id="></div>'
+    console.log(newClub)
+    let targetDiv = document.getElementById(id)
+    newClub.setAttribute("clubID",lookup); // sets the latitude
+    newClub.addEventListener('click', function(){
+      window.location = "./CulturalOrgs.html#" +lookup;
+      //make function to link to clubs -> JS go to URL (window.location)
+      //add anchors
+  }) 
+    targetDiv.appendChild(newClub)
+    return targetDiv
+  }
+
+  // // console.log(document.getElementById(thisID))
+  // if (document.getElementById(id)){
+
+  // }
+
+  
+}
+
+function getClubData(thisurl){
+  fetch(thisurl)
+  .then(response => {
+    return response.json();
+    })
+    .then(data =>{
+                // console.log(data)
+                formatClubData(data)
+        }
+  )
+}
+
+function addStories2(data){
+ console.log(formattedClubData)
+
+  for(let i = 0; i < data.length; i++)
+  {
+    const newDiv = document.createElement("div"); // adds a new Div
+    // B2 = 2
+    let thisID = "story_"+i //story_0; story_1;
+    newDiv.id = thisID; // gives the button a unique id"
+    //=2 +10
+    //=B2 + 10
+    
+    
+    let clubKeywords = 0
+    if (data[i].keywordsusescripttogeneratebasedonstoryalsoclubs){
+      clubKeywords = data[i].keywordsusescripttogeneratebasedonstoryalsoclubs.split(",")
+    }
+  
+    // newButton.setAttribute("lat",lat); // sets the latitude 
+    // newButton.setAttribute("lng",lng); // sets the longitude
+    
+  let theHtmlContent = `<h2>${data[i].name}</h2> <p>${data[i].affirmationstory}</p>`
+  // newButton.innerHTML = title; // gives the button a title
+
+  // <div class="btn-group">
+
+  // clubKeywords.forEach(data => data+1)
+  // console.log(thisID)
+  theHtmlContent += '</div>'
+  // console.log("htmlContent: "+theHtmlContent)
+  newDiv.innerHTML = theHtmlContent
+  const spaceForButtons = document.getElementById('sidebar')
+  spaceForButtons.appendChild(newDiv);//this adds the button to our page.
+  clubKeywords.forEach(club => createClubButton(club,thisID))
+  console.log(clubKeywords)
+
+  }
+  // let theHtmlContent = "<h2>" + name + "</h2>" +
+  // "<p>" + data[i].affirmationstory + "</p>"
+
+}
 
 function addStories(data)
 {
@@ -82,16 +190,29 @@ function addStories(data)
   console.log(divs.length);
   for(let i = 0; i < divs.length; i++)
   {
-    console.log("adding story"); 
-    let name = data[i].name; 
-    if(name == "")
+    console.log("adding story");
+    // let name;
+    // if (data[i].name){
+    //    name = data[i].name;
+    // }
+    // if(name == ""){
+    //   name = "Anonymous"
+    // }
+    // else{
+    //   name = "Anonymous"
+    // }
+    console.log(data)
+    let name = data[i].name;
+    if(name == ""){
       name = "Anonymous"
+    }
+    console.log(data[i].keywordsusescripttogeneratebasedonstoryalsoclubs)
     divs[i].innerHTML = "<h2>" + name + "</h2>" +
     "<p>" + data[i].affirmationstory + "</p>"
     // +'<div class="btn-group"><button onclick="window.location.href=\'About.html\';">Apple</button>  <button onclick="window.location.href=\'About.html\';">Samsung</button>  <button onclick="window.location.href=\'About.html\';">Sony</button>  </div>'
     // ^^ poor attempt to add a button without the quotes escaping;; it did not work :')
     ;
-
+    
     divs[i].setAttribute("lat",data[i].lat); 
     divs[i].setAttribute("lng",data[i].lng);
   }
@@ -134,6 +255,20 @@ function setUpScroll()
 }
 
 
+function formatClubData(clubData){
+  const rows = clubData.feed.entry
+  for(const row of rows) {
+    const formattedClubRow = {}
+    for(const key in row) {
+      if(key.startsWith("gsx$")) {
+            formattedClubRow[key.replace("gsx$", "")] = row[key].$t
+      }
+    }
+    formattedClubData.push(formattedClubRow)
+  }
+  console.log(formattedClubData);
+}
+
 
 function formatData(theData){
     const rows = theData.feed.entry
@@ -147,9 +282,10 @@ function formatData(theData){
       formattedData.push(formattedRow)
     }
     console.log(formattedData);
-
+    
     addLayersToMap(formattedData); //Todo: take these out 
-    addStories(formattedData); 
+    console.log(formattedClubData)
+    addStories2(formattedData); 
     setUpScroll(); 
 }
 
